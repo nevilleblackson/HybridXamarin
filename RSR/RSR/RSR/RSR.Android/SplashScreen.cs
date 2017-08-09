8433290da64a5a190fd721486de50d58fef796c2
nevilleblackson <nblackson@roc-dev.com>
2017-08-09 11:05:29 +0200

    "reset"

---
+++ RSR/RSR/RSR/RSR.Android/SplashScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+using Android.Support.V7.App;
+using Android.Util;
+
+namespace RSR.Droid
+{
+
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true, Label = "SplashScreen", Icon = "@drawable/ic_launcher")]
+    public class SplashActivity : AppCompatActivity
+    {
+        //static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            //Log.Debug(TAG, "SplashActivity.OnCreate");
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            await Task.Delay(100);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
---
+++ RSR/RSR/RSR/RSR/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RSR
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public void StartMap(object sender, EventArgs args) {
+
+        }
+
+    }
+}
